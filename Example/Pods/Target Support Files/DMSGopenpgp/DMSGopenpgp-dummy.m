ef42c59e2b19f542f0ea0fe6a1abd31d224f4a5d
jk234ert <ugling88@hotmail.com>
2019-08-01 14:15:19 +0800

    init podspec

---
+++ Example/Pods/Target Support Files/DMSGopenpgp/DMSGopenpgp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DMSGopenpgp : NSObject
+@end
+@implementation PodsDummy_DMSGopenpgp
+@end
--- build.sh
+++ build.sh
@@ -12,11 +12,11 @@ mkdir -p $IOS_OUT
 printf "\e[0;32mStart Building iOS framework .. Location: ${IOS_OUT} \033[0m\n\n"
 PACKAGE_PATH=github.com/DimensionDev/gopenpgp
 
-gomobile bind -target ios -o ${IOS_OUT}/crypto.framework $PACKAGE_PATH/crypto $PACKAGE_PATH/helper $PACKAGE_PATH/armor $PACKAGE_PATH/constants $PACKAGE_PATH/models $PACKAGE_PATH/subtle
+gomobile bind -target ios -o ${IOS_OUT}/DMSOpenPGP.framework $PACKAGE_PATH/DMSOpenPGP $PACKAGE_PATH/crypto $PACKAGE_PATH/helper $PACKAGE_PATH/armor $PACKAGE_PATH/constants $PACKAGE_PATH/models $PACKAGE_PATH/subtle
 
 printf "\e[0;32mStart Building Android lib .. Location: ${ANDROID_OUT} \033[0m\n\n"
 
-gomobile bind -target android -javapkg com.proton.gopenpgp -o ${ANDROID_OUT}/gopenpgp.aar $PACKAGE_PATH/crypto $PACKAGE_PATH/helper $PACKAGE_PATH/armor $PACKAGE_PATH/constants $PACKAGE_PATH/models $PACKAGE_PATH/subtle 
+gomobile bind -target android -javapkg com.dimension.DMSOpenPGP -o ${ANDROID_OUT}/DMSOpenPGP.aar $PACKAGE_PATH/DMSOpenPGP $PACKAGE_PATH/crypto $PACKAGE_PATH/helper $PACKAGE_PATH/armor $PACKAGE_PATH/constants $PACKAGE_PATH/models $PACKAGE_PATH/subtle 
 
 printf "\e[0;32mInstalling frameworks. \033[0m\n\n"
 
