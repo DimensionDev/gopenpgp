67add6cc3221b0d2519e93a33b6a1c0aa9f14dbf
CMK <cirno.mainasuk@gmail.com>
2020-03-21 21:26:54 +0800

    feat: add KeyRing factory method. Add KeyEntity append interface. Update pod to 0.0.18

--- build.sh
+++ build.sh
@@ -15,15 +15,15 @@ printf "\e[0;32mStart Building iOS framework .. Location: ${IOS_OUT} \033[0m\n\n
 PACKAGE_PATH=github.com/DimensionDev/gopenpgp
 GO_CRYPTO_OPENPGP=golang.org/x/crypto/openpgp
 
-# gomobile bind -ldflags=-w -target ios -o ${IOS_OUT}/DMSGoPGP.framework $PACKAGE_PATH/DMSGoPGP \
-# $PACKAGE_PATH/crypto $PACKAGE_PATH/helper $PACKAGE_PATH/armor $PACKAGE_PATH/constants
+gomobile bind -ldflags=-w -target ios -o ${IOS_OUT}/DMSGoPGP.framework $PACKAGE_PATH/DMSGoPGP \
+$PACKAGE_PATH/crypto $PACKAGE_PATH/helper $PACKAGE_PATH/armor $PACKAGE_PATH/constants
 
-# printf "\e[0;32mStart Building Android lib .. Location: ${ANDROID_OUT} \033[0m\n\n"
+printf "\e[0;32mStart Building Android lib .. Location: ${ANDROID_OUT} \033[0m\n\n"
 
-# gomobile bind -target android -javapkg com.dimension.DMSGoPGP -o ${ANDROID_OUT}/DMSGoPGP.aar $PACKAGE_PATH/DMSGoPGP \
-# $PACKAGE_PATH/crypto $PACKAGE_PATH/helper $PACKAGE_PATH/armor $PACKAGE_PATH/constants
+gomobile bind -target android -javapkg com.dimension.DMSGoPGP -o ${ANDROID_OUT}/DMSGoPGP.aar $PACKAGE_PATH/DMSGoPGP \
+$PACKAGE_PATH/crypto $PACKAGE_PATH/helper $PACKAGE_PATH/armor $PACKAGE_PATH/constants
 
-# printf "\e[0;32mInstalling frameworks. \033[0m\n\n"
+printf "\e[0;32mInstalling frameworks. \033[0m\n\n"
 
 printf "\e[0;32mStart Building WASM lib .. Location: ${WASM_OUT} \033[0m\n\n"
 
